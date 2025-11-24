d76ab669cf39aad9d18a4831b7de525f1070fd10
Zaheer Abbas Khan <153213111+ZDrago-afk@users.noreply.github.com>
2025-11-24 21:13:53 +0530

    Implement scaling of coordinates in graphics
    Added scaling functionality to the graphics program and displayed new coordinates.

--- scaling.cpp
+++ scaling.cpp
@@ -11,19 +11,15 @@ void multiplyMatrix(int mat[3][3], int point[3], int result[3]) {
         }
     }
 }
-
 int main() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
-
     int x[3] = {100, 150, 200};
     int y[3] = {100,  50, 100};
-
     setcolor(WHITE);
     line(x[0], y[0], x[1], y[1]);
     line(x[1], y[1], x[2], y[2]);
     line(x[2], y[2], x[0], y[0]);
-
     int Sx, Sy;
     cout << "Enter Sx and Sy: ";
     cin >> Sx >> Sy;
@@ -33,7 +29,7 @@ int main() {
         {0,  Sy, 0},
         {0,  0,  1}
     };
-
+    
     int newx[3], newy[3];
 
     for (int i = 0; i < 3; i++) {
@@ -45,12 +41,18 @@ int main() {
         newx[i] = result[0];
         newy[i] = result[1];
     }
-
+    cout<<"new coordinates are: \n";
+    for(int i=0;i<3;i++){
+        cout<<"("<<newx[i]<<","<<newy[i]<<")\n";
+    }
+    setcolor(BLACK);
+    line(x[0], y[0], x[1], y[1]);
+    line(x[1], y[1], x[2], y[2]);
+    line(x[2], y[2], x[0], y[0]);
     setcolor(YELLOW);
     line(newx[0], newy[0], newx[1], newy[1]);
     line(newx[1], newy[1], newx[2], newy[2]);
     line(newx[2], newy[2], newx[0], newy[0]);
-
     getch();
     closegraph();
     return 0;
