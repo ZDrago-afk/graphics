1e48a4a028f1605dd163a4ea2f8a7ab258ace15a
Zaheer Abbas Khan <153213111+ZDrago-afk@users.noreply.github.com>
2025-11-25 05:32:08 +0530

    Implement boundary fill algorithm in C++

---
+++ boundary_fill.cpp
@@ -0,0 +1,24 @@
+#include <graphics.h>
+#include <conio.h>
+
+void boundaryFill(int x, int y, int fillColor, int boundaryColor) {
+    if (getpixel(x, y) != boundaryColor && getpixel(x, y) != fillColor) {
+        putpixel(x, y, fillColor);
+        boundaryFill(x + 1, y, fillColor, boundaryColor);
+        boundaryFill(x - 1, y, fillColor, boundaryColor);
+        boundaryFill(x, y + 1, fillColor, boundaryColor);
+        boundaryFill(x, y - 1, fillColor, boundaryColor);
+    }
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    rectangle(100, 100, 300, 250);
+    boundaryFill(150, 150, RED, WHITE);
+
+    getch();
+    closegraph();
+    return 0;
+}
